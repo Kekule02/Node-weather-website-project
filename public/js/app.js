6e4ed251c1459583230d3a24712bf28844d620ce
Kekule02 <azeezlawal2002@gmail.com>
2022-08-02 14:41:01 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,19 +18,17 @@ weatherForm.addEventListener("submit", (e) => {
   // Removing the previous Forecast Data message
   messageTwo.textContent = "";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          //Rendering the Error message
-          messageOne.textContent = data.error;
-        } else {
-          // Rendering the Forecast Data
-          messageOne.textContent = `This forecast is for ${data.location}.`;
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        //Rendering the Error message
+        messageOne.textContent = data.error;
+      } else {
+        // Rendering the Forecast Data
+        messageOne.textContent = `This forecast is for ${data.location}.`;
 
-          messageTwo.textContent = `${data.forecastData}`;
-        }
-      });
-    }
-  );
+        messageTwo.textContent = `${data.forecastData}`;
+      }
+    });
+  });
 });
